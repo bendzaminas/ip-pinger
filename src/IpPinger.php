41d9defae79f97e7c05f327cea384206f28feb2c
bendzaminas <39025147+bendzaminas@users.noreply.github.com>
2021-02-10 18:13:42 +0200

    Update IpPinger.php

--- src/IpPinger.php
+++ src/IpPinger.php
@@ -2,113 +2,44 @@
 
 namespace BCode\IpPinger;
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use BCode\IpPinger\Entity\IpInterface;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Client;
-use GuzzleHttp\Promise\EachPromise;
-use GuzzleHttp\Promise\PromiseInterface;
+use Spatie\Async\Pool;
+use JJG\Ping;
 
 class IpPinger
 {
-    /**
-     * @var ClientInterface
-     */
-    protected $client;
-	
-	/**
-     * @var array
-     */
-    protected $options;
-	
-	/**
-     * @var array
-     */
-    protected $requestOptions;
-	
-	/**
-     * @var array
-     */
-    protected $ips;
+    protected $pool;
 
-    /**
-     * @param array|null $options
-     * @param array|null $requestOptions
-     * @param ClientInterface|null $guzzle
-     */
     public function __construct(
-        ?array $options = [],
-        ?array $requestOptions = [],
-        ?ClientInterface $guzzle = null
     ) {
-	$this->options = $options + $this->getDefaultOptions();
-        $this->requestOptions = $requestOptions + $this->getDefaultRequestOptions();
-        $this->client = $guzzle ?? new Client($this->requestOptions);
-	$this->ips = [];
+	$this->pool = Pool::create();
     }
 
-    /**
-     * @param IpInterface[] $ips
-     * @return IpInterface[]
-     */
     public function pingIps(array $ips): array
-    {
-        $ipIndexMap = array_keys($ips);
-		
+    {	    
+	$pingedIps = [];
+	    
+    	foreach ($ips as $ip) {
 
-        $eachPromise = new EachPromise($this->getPromiseGenerator($ips)(), [
-            'concurrency' => $this->options['concurrency'],
-            'fulfilled' => function (ResponseInterface $response, int $index) use ($ips, &$validIps, $ipIndexMap): void {
-               
-		$ip = $ips[$ipIndexMap[$index]];
-					
-                $this->ips[] = ["status" => true, "ip" => $ip];
-        
-            },
-            'rejected' => function (\Exception $reason, int $index) use ($ips, $ipIndexMap): void {
-            	
-		$ip = $ips[$ipIndexMap[$index]];
-					
-                $this->ips[] = ["status" => false, "ip" => $ip];	
-		    
-            }
-        ]);
+        	$this->pool->add(function () use ($ip) {
+			
+                       $ping = new Ping($ip->getIp(), 1, 1);
+			
+                       $latency = $ping->ping('fsockopen');
+			
+                       $ip->setLatency($latency);
+			
+		       return $ip;
+			
+                })->then(function ($ip) use (&$pingedIps) {
+                      $pingedIps[] = $ip;
+                })->catch(function (Throwable $exception) use (&$pingedIps)  {
+		      $ip->setException($exception);
+		      $pingedIps[] = $ip;
+                });
+         }
 
-        $eachPromise->promise()->wait();
-        return $this->ips;
-    }
-
-    /**
-     * @param IpsInterface[] $ips
-     * @return \Closure
-     */
-    protected function getPromiseGenerator(array $ips): \Closure
-    {
-        return function () use ($ips): \Generator {
-            foreach ($ips as $ip) {
-                yield $this->client->headAsync((string)$ip);
-            }
-        };
-    }
-
-    /**
-     * @return array
-     */
-    protected function getDefaultOptions(): array
-    {
-        return [
-            'concurrency' => 50
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    protected function getDefaultRequestOptions(): array
-    {
-        return [
-            'timeout' => 1
-        ];
+         $pool->wait();
+	    
+	 return $pingedIps;
     }
 }
